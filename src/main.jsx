c545917881b98dc579e3317c2f1920366f9a14db
Смирнов Сергей Александрович <sea.smirnov@bft.ru>
2025-12-25 17:26:53 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 );
